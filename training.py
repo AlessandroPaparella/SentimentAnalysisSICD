3f661278121fe5bfbebd75c6ffb8e3123bd08525
AlessandroPaparella <61472307+AlessandroPaparella@users.noreply.github.com>
2021-07-15 11:03:21 +0200

    Create training.py
    Script per google colab per la specilizzazione del modello di bert su dataset sentiment140

---
+++ training.py
@@ -0,0 +1,167 @@
+from transformers import BertTokenizer, TFBertForSequenceClassification
+from transformers import InputExample, InputFeatures
+
+model = TFBertForSequenceClassification.from_pretrained("bert-base-uncased")
+tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+
+import tensorflow as tf
+import pandas as pd
+
+from google.colab import drive
+drive.mount('/content/drive')
+
+#get dataset from google drive
+import tarfile 
+file = tarfile.open('drive/MyDrive/140.tar.gz') 
+file.extractall('./') 
+file.close() 
+
+# The shutil module offers a number of high-level 
+# operations on files and collections of files.
+import os
+import shutil
+# Create main directory path ("/140")
+dataset="./140"
+main_dir = os.path.join(os.path.dirname(dataset), '140')
+# Create sub directory path ("/140/train")
+train_dir = os.path.join(main_dir, 'train')
+#print classes
+print(os.listdir(train_dir))
+
+# We create a training dataset and a validation 
+# dataset from our "140/train" directory with a 80/20 split.
+train = tf.keras.preprocessing.text_dataset_from_directory(
+    '140/train', batch_size=20000, validation_split=0.2, 
+    subset='training', seed=123)
+test = tf.keras.preprocessing.text_dataset_from_directory(
+    '140/test', batch_size=20000, validation_split=0.2, 
+    subset='validation', seed=123)
+
+
+#Preparing dataset for bert...
+
+for i in train.take(1):
+  train_feat = i[0].numpy()
+  train_lab = i[1].numpy()
+
+train = pd.DataFrame([train_feat, train_lab]).T
+train.columns = ['DATA_COLUMN', 'LABEL_COLUMN']
+train['DATA_COLUMN'] = train['DATA_COLUMN'].str.decode("utf-8")
+train.head()
+
+for j in test.take(1):
+  test_feat = j[0].numpy()
+  test_lab = j[1].numpy()
+
+test = pd.DataFrame([test_feat, test_lab]).T
+test.columns = ['DATA_COLUMN', 'LABEL_COLUMN']
+test['DATA_COLUMN'] = test['DATA_COLUMN'].str.decode("utf-8")
+test.head()
+
+def convert_data_to_examples(train, test, DATA_COLUMN, LABEL_COLUMN): 
+  train_InputExamples = train.apply(lambda x: InputExample(guid=None, # Globally unique ID for bookkeeping, unused in this case
+                                                          text_a = x[DATA_COLUMN], 
+                                                          text_b = None,
+                                                          label = x[LABEL_COLUMN]), axis = 1)
+
+  validation_InputExamples = test.apply(lambda x: InputExample(guid=None, # Globally unique ID for bookkeeping, unused in this case
+                                                          text_a = x[DATA_COLUMN], 
+                                                          text_b = None,
+                                                          label = x[LABEL_COLUMN]), axis = 1)
+  
+  return train_InputExamples, validation_InputExamples
+
+  train_InputExamples, validation_InputExamples = convert_data_to_examples(train, 
+                                                                           test, 
+                                                                           'DATA_COLUMN', 
+                                                                           'LABEL_COLUMN')
+  
+def convert_examples_to_tf_dataset(examples, tokenizer, max_length=128):
+    features = [] # -> will hold InputFeatures to be converted later
+
+    for e in examples:
+        # Documentation is really strong for this method, so please take a look at it
+        input_dict = tokenizer.encode_plus(
+            e.text_a,
+            add_special_tokens=True,
+            max_length=max_length, # truncates if len(s) > max_length
+            return_token_type_ids=True,
+            return_attention_mask=True,
+            pad_to_max_length=True, # pads to the right by default # CHECK THIS for pad_to_max_length
+            truncation=True
+        )
+
+        input_ids, token_type_ids, attention_mask = (input_dict["input_ids"],
+            input_dict["token_type_ids"], input_dict['attention_mask'])
+
+        features.append(
+            InputFeatures(
+                input_ids=input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids, label=e.label
+            )
+        )
+
+    def gen():
+        for f in features:
+            yield (
+                {
+                    "input_ids": f.input_ids,
+                    "attention_mask": f.attention_mask,
+                    "token_type_ids": f.token_type_ids,
+                },
+                f.label,
+            )
+
+    return tf.data.Dataset.from_generator(
+        gen,
+        ({"input_ids": tf.int32, "attention_mask": tf.int32, "token_type_ids": tf.int32}, tf.int64),
+        (
+            {
+                "input_ids": tf.TensorShape([None]),
+                "attention_mask": tf.TensorShape([None]),
+                "token_type_ids": tf.TensorShape([None]),
+            },
+            tf.TensorShape([]),
+        ),
+    )
+
+
+DATA_COLUMN = 'DATA_COLUMN'
+LABEL_COLUMN = 'LABEL_COLUMN'
+
+
+train_InputExamples, validation_InputExamples = convert_data_to_examples(train, test, DATA_COLUMN, LABEL_COLUMN)
+print(train_InputExamples)
+train_data = convert_examples_to_tf_dataset(list(train_InputExamples), tokenizer)
+train_data = train_data.shuffle(100).batch(32).repeat(2)
+
+validation_data = convert_examples_to_tf_dataset(list(validation_InputExamples), tokenizer)
+validation_data = validation_data.batch(32)
+
+#Training 
+
+model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=3e-5, epsilon=1e-08, clipnorm=1.0), 
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True), 
+              metrics=[tf.keras.metrics.SparseCategoricalAccuracy('accuracy')])
+
+model.fit(train_data, epochs=2, validation_data=validation_data)
+
+
+#Save and zip
+
+def save(model, tokenizer):
+  output_dir = '/my_model'
+  if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+    print("Saving model to {}".format(output_dir))
+    model.save_pretrained(output_dir)
+    tokenizer.save_pretrained(output_dir)
+
+save(model, tokenizer)
+
+def tardir(path, tar_name):
+    with tarfile.open(tar_name, "w:gz") as tar_handle:
+        for root, dirs, files in os.walk(path):
+            for file in files:
+                tar_handle.add(os.path.join(root, file))
+tardir('./my_model', 'mymodel.tar.gz')
+tar.close()
